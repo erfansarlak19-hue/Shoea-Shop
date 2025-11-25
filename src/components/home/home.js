1081b73cb37930470ed1d482a4e1ffedab98485d
erfansarlak19-hue <erfansarlak19@gmail.com>
2025-11-26 01:24:30 +0330

    fix: fix some bug and chenge it

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,4 @@
 import { El } from "../utils/el";
-import { router } from "../utils/router";
 import { Footer } from "./footer";
 import { Header } from "./header";
 
@@ -27,70 +26,6 @@ export function Home() {
 								className: "font-semibold text-[#152536]",
 								innerText: "See All",
 							}),
-						],
-					}),
-					El({
-						element: "div",
-						className:
-							"flex pl-5 items-center gap-3 overflow-x-auto hide-scrollbar h-18 bg-white",
-						children: [
-							El({
-								element: "button",
-								className:
-									"shrink-0 border-2 border-[#343a40] h-10 w-15 text-[#343a40] text-center font-bold rounded-3xl",
-								innerText: "All",
-							}),
-						],
-					}),
-					El({
-						element: "div",
-						className:
-							"pl-5 pr-5 h-auto grid grid-cols-2 gap-5 overflow-auto hide-scrollbar",
-						children: [
-							El({
-								element: "div",
-								className: "shrink-0 h-61 w-46 flex flex-col gap-2",
-								eventListener: [
-									{
-										event: "click",
-										callback: () => {
-											router.navigate("/product");
-										},
-									},
-								],
-								children: [
-									El({
-										element: "div",
-										className:
-											"w-46 h-46 rounded-3xl  bg-[#f3f3f3] flex justify-center items-center",
-										children: [
-											El({
-												element: "img",
-												src: "",
-												className: "w-36 h-36 object-contain rounded-3xl",
-											}),
-										],
-									}),
-									El({
-										element: "div",
-										className: "flex flex-col gap-1",
-										children: [
-											El({
-												element: "p",
-												className:
-													"text-[18px] font-bold text-[#152536] truncate",
-												innerText: "K-Swiss ista Train saer",
-											}),
-											El({
-												element: "p",
-												className: "text-[16px] font-semibold text-[#152536]",
-												innerText: "$ 85.00",
-											}),
-										],
-									}),
-								],
-							}),
-							
 						],
 					}),
 				],
